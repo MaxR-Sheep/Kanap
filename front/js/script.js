bd0ff4226160c2da71af969aca27d3e8b4a02495
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-02-13 15:30:35 +0100

    chemin produit

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,14 @@
 const requeteAllProduit =  fetch("http://localhost:3000/api/products");
  // récuperer la réponse en JSON
 
-function recuperationsProduits() {
+function recuperationsProduits() { // fonction pour récuperer les informations en JSON
     requeteAllProduit
     .then(function(res) {
         if (res.ok) {
             return res.json();
         }
     })
-    .then (function(value) {
+    .then (function(value) {// appel un function qui vas afficher les valeurs récupérer 
         const listProduct = value;
         afficheProduits(listProduct)
         console.log(listProduct);
@@ -19,49 +19,41 @@ function recuperationsProduits() {
     });
     
 }
-recuperationsProduits();
-
+console.log(recuperationsProduits);
+recuperationsProduits(); // appel de la fonction
 
 console.log(requeteAllProduit);
 
-function afficheProduits(listProduct){
+function afficheProduits(listProduct){// function qui vas afficher chaque element pour l'HTML
     listProduct.forEach((product,index) => {
         console.log("numero du produit:" + index + ", nom du produit: " + product.name);
         
-        sectionItems = document.getElementById("items")
+        sectionItems = document.getElementById("items")// création d'un ID 
 
-        let newLien = document.createElement("a");
-        newLien.setAttribute("href",`./product.html?id=${product._id}`);
+        let newLien = document.createElement("a"); // création d'une card avec un lien cliquable 
+        newLien.setAttribute("href",`./product.html?id=${product._id}`);// permet que chaque lien soit unique selon la carte cliquer
         sectionItems.appendChild(newLien);
 
         let newArticle = document.createElement("article");
         newLien.appendChild(newArticle);
 
-        let newImages = document.createElement("img");
+        let newImages = document.createElement("img");// affichage de l'image avec sa source et son alt
         newImages.setAttribute("src", product.imageUrl);
         newImages.setAttribute("alt", product.altTxt);
         newArticle.appendChild(newImages);
 
-        let newH3 = document.createElement("h3");
+        let newH3 = document.createElement("h3");// titre de la card
         newH3.setAttribute("class","productName");
         newH3.innerText = product.name;
         newArticle.appendChild(newH3);
 
-        let newP = document.createElement("p");
+        let newP = document.createElement("p");// texte de la card
         newP.setAttribute("class","productDescription");
         newP.innerText = product.description;
         newArticle.appendChild(newP);
     });
 };
 
-let pageArticle = ""
-
-let search_params = new URLSearchParams(pageArticle);
-console.log(pageArticle);
-if (search_params.has("id")){
-    let identifant = search_params.get("id");
-    console.log(identifant);
-};
 
 
 
