7f4b3271008b612b9544c17eba3afb0629f46c14
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-02-27 12:28:59 +0100

    ajout commentaire , element panier afficher

--- front/js/script.js
+++ front/js/script.js
@@ -3,17 +3,17 @@ const requeteAllProduit =  fetch("http://localhost:3000/api/products");
 
 function recuperationsProduits() { // fonction pour récuperer les informations en JSON
     requeteAllProduit
-    .then(function(res) {
-        if (res.ok) {
+    .then(function(res) { // promesse pour une fonction qui attend une réponse
+        if (res.ok) { //si la réponse est ok
             return res.json();// retourne la réponse en JSON
         }
     })
     .then (function(value) {// appel un function qui vas afficher les valeurs récupérer 
-        const listProduct = value;
-        afficheProduits(listProduct)
+        const listProduct = value; // notre constante listProduct est égale a une valeur
+        afficheProduits(listProduct) // on appel la fonction
         console.log(listProduct);
     })
-    .catch(function(err) {
+    .catch(function(err) { // sinon on appel un fonction erreur pour nous la rapporté
         const error = "Une erreur est survenue" + " " + err;
     return  error;
     });
@@ -25,25 +25,25 @@ recuperationsProduits(); // appel de la fonction
 console.log(requeteAllProduit);
 
 function afficheProduits(listProduct){// function qui vas afficher chaque element pour l'HTML
-    listProduct.forEach((product,index) => {
+    listProduct.forEach((product,index) => { // on recherhce pour chque produit ses éléments
         console.log("numero du produit:" + index + ", nom du produit: " + product.name);
         
         sectionItems = document.getElementById("items")// création d'un ID 
 
         let newLien = document.createElement("a"); // création d'une card avec un lien cliquable 
         newLien.setAttribute("href",`./product.html?id=${product._id}`);// permet que chaque lien soit unique selon la carte cliquer
-        sectionItems.appendChild(newLien);
+        sectionItems.appendChild(newLien);//newlien devient l'enfant de sectionItems
 
         let newArticle = document.createElement("article");
         newLien.appendChild(newArticle);
 
         let newImages = document.createElement("img");// affichage de l'image avec sa source et son alt
-        newImages.setAttribute("src", product.imageUrl);
+        newImages.setAttribute("src", product.imageUrl);//  on récupere l'image dans l'api 
         newImages.setAttribute("alt", product.altTxt);
         newArticle.appendChild(newImages);
 
         let newH3 = document.createElement("h3");// titre de la card
-        newH3.setAttribute("class","productName");
+        newH3.setAttribute("class","productName");//on attribut au titre une class= productName
         newH3.innerText = product.name;
         newArticle.appendChild(newH3);
 
